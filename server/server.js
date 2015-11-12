2decb37dea3d00b806da57ee1ef05399a1e39043
Tad <tad@whitakerresearch.com>
2015-11-11 18:30:23 -0800

    Added process envir variable.

--- server/server.js
+++ server/server.js
@@ -15,26 +15,11 @@ mongoose.connect(mongoURI); // connect to mongo database named drone
 require('./config/middleware.js')(app, express);
 
 // export our app for testing and flexibility, required by index.js
-app.listen(port);
+http.listen(process.env.PORT || 3333, function(){
+  console.log('listening on', http.address().port);
+});
 
 
 console.log("Listening on port: ", port);
 
 module.exports = app;
-
-
-/* Walkthrough of the server
-
-  Express, mongoose, and our server are initialzed here
-  Next, we then inject our server and express into our config/middleware.js file for setup.
-    We also exported our server for easy testing
-
-  middleware.js requires all express middleware and sets it up
-  our authentication is set up there as well
-  we also create individual routers for are two main features, links and users
-  each feature has its own folder with a model, controller, and route file
-    the respective file is required in middleware.js and injected with its mini router
-    that route file then requires the respective controller and sets up all the routes
-    that controller then requires the respective model and sets up all our endpoints which respond to requests
-
-*/
